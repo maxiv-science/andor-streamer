06a831b30516fdc44aa4e93791663abf8db2951e
Felix Engelmann <felix.engelmann@maxiv.lu.se>
2023-02-06 15:52:46 +0100

    change package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='andor-streamer',
+    name='dev-andor3',
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     packages=find_packages(),
     install_requires=['libdaq', 'pytango', 'pyzmq'],
     entry_points = {
-        'console_scripts': ['Andor3 = andor_streamer.Andor3:main',]
+        'console_scripts': ['Andor3 = dev_andor3.Andor3:main',]
     }
 ) 
  
